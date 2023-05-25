efc9e2b1a5bc31e28613987975baab37b4654cbd
Rugnus <sungur.gasanov@mail.ru>
2023-05-25 16:12:27 +0300

    Registration and auth with sync table added

--- src/pages/api/login.js
+++ src/pages/api/login.js
@@ -1,15 +1,39 @@
 import { prisma } from "../../../server/db/client";
-import { Users } from "../data/users";
 // import { Users } from "../data/users";
-export default function handler(req, res) {
-    const users = prisma.user.findMany()
+export default async function handler(req, res) {
+    const users = await prisma.user.findMany()
+    const pUsers = JSON.parse(JSON.stringify(users))
+    console.log("Parsed users: " + pUsers)
+    // console.log(users)
+    // const login = users?.map(user => user.email)
+    // console.log(login)
+    // const password = users?.map(user => user.password)
+    // console.log(password)
     try {
         if (req.method !== 'POST') {
             res.status(405).send({ message: 'Only POST requests allowed' })
             return
         }
         const body = JSON.parse(JSON.stringify(req.body))
-        const user = Users.find((user) => user.email === body.email && user.password === parseInt(body.password));
+        // console.log("Fields values" + body.email + body.password)
+        // const user = currUser
+        //   console.log(user)
+        // const user = await prisma.user.findUnique({
+        //     where: {
+        //       email: body.email,
+        //       password: parseInt(body.password)
+        //     },
+        //   })
+        // console.log(user.email)
+        // const user = parsedUsers.find((user) => user.email === body.email && user.password === parseInt(body.password))
+        // const user = users.find((user) => user.email === body.email && user.password === parseInt(body.password))
+        // console.log(user.email)
+        // console.log(user.password)
+        const user = pUsers.find((user1) => user1.email === body.email && user1.password === body.password);
+        console.log(user)
+        // const users1 = users.forEach((user) => user.email === body.email && user.password === parseInt(body.password))
+        // console.log("User: " + user)
+        // console.log("User1: " + users1)
         if (!user) {
             res.status(404).send({ message: 'User does not exit!' })
             return
@@ -21,3 +45,13 @@ export default function handler(req, res) {
         return
     }
 };
+
+export async function getServerSideProps() {
+    const users = await prisma.user.findMany()
+    return {
+        props: {
+            users: JSON.parse(JSON.stringify(users))
+        }
+    }
+}
+
--- src/pages/api/users.js
+++ src/pages/api/users.js
@@ -3,6 +3,7 @@ import { prisma } from "../../../server/db/client"
 
 export default async function handler(req, res) {
     const {method} = req
+    const {userID, login, firstName, lastName, email, password} = req.body
 
     switch(method){
         case 'POST':
@@ -21,8 +22,16 @@ export default async function handler(req, res) {
             console.log(newID)
             res.status(201).json(post)
             break
+        case 'PUT':
+            console.log(req.body.userID)
+            // console.log(getUserID + walletAdress)
+            const result = await prisma.user.update({
+                where: {userID: req.body.userID},
+                data: {walletAdress: req.body.walletAdress},
+            })
+            res.json(result)
         default:
-            res.setHeader("Allow", ['POST'])
+            res.setHeader("Allow", ['POST', 'PUT'])
             res.status(405).end(`Method ${method} Now Allowed`)
     }
 }
