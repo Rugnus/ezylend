c1c759b806146ba1b23832ab292a024356adc031
Rugnus <sungur.gasanov@mail.ru>
2023-05-30 22:34:00 +0300

    Frontend for supply and stake pages made

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -9,11 +9,12 @@ import {ethers} from 'ethers'
 import {prisma} from '../../server/db/client'
 import axios from 'axios'
 
-export default function Dashboard({posts, filteredCoins}) {
+export default function Dashboard({posts, filteredCoins, supplies}) {
     const { data: session, status } = useSession()
     console.log(session?.user?.userID)
     const [userAccount, setUserAccount] = useState("")
     const [balance, setBalance] = useState(1.25)
+    const [supplyValue, setSupplyValue] = useState(0)
     const [value, setValue] = useState(1)
     console.log([filteredCoins][0].bitcoin.usd)
     // const handleSubmit = async ({language, code}) => {
@@ -52,9 +53,7 @@ export default function Dashboard({posts, filteredCoins}) {
     }
 
     const user = session?.user;
-    const updateUser = async () => {
-        
-    }
+    const currUID = session?.user?.userID
 
     // const getBalance = (account) => {
     //     window.ethereum.request({method: "eth_getBalance", params: [account, "latest"],}).then((balance) => {
@@ -68,7 +67,41 @@ export default function Dashboard({posts, filteredCoins}) {
         window.location.reload()
     }
 
-    
+    const newDate = new Date()
+
+    const postBTCSupply = async ({userID = currUID, supplyAmount=parseFloat(supplyValue), currency = "BTC",  createdAt = newDate}) => {
+        
+        const {data} = await axios.post('api/supplies', {
+            userID,
+            supplyAmount,
+            currency,
+            createdAt
+        })
+        console.log(data)
+        location.reload()
+    }
+
+    const postETHSupply = async ({userID = currUID, supplyAmount=parseFloat(supplyValue), currency = "ETH",  createdAt = newDate}) => { 
+        const {data} = await axios.post('api/supplies', {
+            userID,
+            supplyAmount,
+            currency,
+            createdAt
+        })
+        console.log(data)
+        location.reload()
+    }
+
+    const postUSDTSupply = async ({userID = currUID, supplyAmount=parseFloat(supplyValue), currency = "USDT",  createdAt = newDate}) => {
+        const {data} = await axios.post('api/supplies', {
+            userID,
+            supplyAmount,
+            currency,
+            createdAt
+        })
+        console.log(data)
+        location.reload()
+    }
 
 
     return (
@@ -84,13 +117,15 @@ export default function Dashboard({posts, filteredCoins}) {
                     <div className={styles.dashboardFirstSection}>
                         <div className={styles.currentSupplies}>
                             <h3>Ваши вклады</h3>
-                            <p>На данный момент у вас нет вкладов</p>
-                            <ul></ul>
-                            {posts?.map(post => (
-                                <li key={post.id}>
-                                    {post.title}
-                                </li>
-                            ))}
+                            {/* <p>На данный момент у вас нет вкладов</p> */}
+                            <ul>
+                                {supplies?.map(supply => (
+                                    supplies.userID == currUID.userID && <li key={supply.supplyID}>
+                                        {supply.supplyAmount}
+                                        {supply.currency}
+                                    </li>
+                                ))}
+                            </ul>
                         </div>
                         <div className={styles.currentBalance}>
                             <h3>Текущий баланс:</h3>
@@ -122,7 +157,7 @@ export default function Dashboard({posts, filteredCoins}) {
                             <table>
                                 <tr>
                                     <th>Актив</th>
-                                    <th>APY</th>
+                                    <th>APR</th>
                                     <th>Может быть залогом</th>
                                     <th></th>
                                     <th></th>
@@ -131,22 +166,22 @@ export default function Dashboard({posts, filteredCoins}) {
                                     <td><Image src={"technologies/btc.svg"} width={25} height={25}/>  Bitcoin</td>
                                     <td>1.5%</td>
                                     <td>+</td>
-                                    <td><a className={styles.supplyButton}>Вложить</a></td>
+                                    <td><a className={styles.supplyButton} href="#popup-box4">Вложить</a></td>
                                     <td><a className={styles.tokenInfoButton} href='#popup-box'>Об активе</a></td>
                                 </tr>
                                 <tr>
                                     <td><Image src={"technologies/eth.svg"} width={25} height={25}/>  Ethereum</td>
                                     <td>3.2%</td>
                                     <td>+</td>
-                                    <td><a className={styles.supplyButton}>Вложить</a></td>
-                                    <td><a className={styles.tokenInfoButton}>Об активе</a></td>
+                                    <td><a className={styles.supplyButton} href="#popup-box5">Вложить</a></td>
+                                    <td><a className={styles.tokenInfoButton} href="#popup-box2">Об активе</a></td>
                                 </tr>
                                 <tr>
                                     <td><Image src={"technologies/usdt.svg"} width={25} height={25}/>  USDT</td>
                                     <td>1.78%</td>
                                     <td>+</td>
-                                    <td><a className={styles.supplyButton}>Вложить</a></td>
-                                    <td><a className={styles.tokenInfoButton}>Об активе</a></td>
+                                    <td><a className={styles.supplyButton} href="#popup-box6">Вложить</a></td>
+                                    <td><a className={styles.tokenInfoButton} href='#popup-box3'>Об активе</a></td>
                                 </tr>
                             </table>
                     </div>
@@ -157,7 +192,7 @@ export default function Dashboard({posts, filteredCoins}) {
                             <div className={styles.tokenTextInfo}>
                                 <div className={styles.tokenFirstRow}>
                                     <span>Всего вложено: ? из ?</span>
-                                    <span>APY: 1,5% </span>
+                                    <span>APR: 1,5% </span>
                                 </div>
                                 <div className={styles.tokenSecondRow}>
                                     <span>Всего заимствовано: ? из ?</span>
@@ -167,8 +202,82 @@ export default function Dashboard({posts, filteredCoins}) {
                                     <span>Коэффициент использования: ?%</span>
                                 </div>
                             </div>
+                            <a className={styles.boxClose} href="#">x</a>
                         </div>
-                        <a className={styles.boxClose} href="#">x</a>
+                    </div>
+                    <div className={styles.tokenInfo} id="popup-box2">
+                        <div className={styles.popupContent} >
+                            <h4><Image src={"technologies/eth.svg"} width={35} height={35}/>  Ethereum</h4>
+                            
+                            <div className={styles.tokenTextInfo}>
+                                <div className={styles.tokenFirstRow}>
+                                    <span>Всего вложено: ? из ?</span>
+                                    <span>APR: 3,2% </span>
+                                </div>
+                                <div className={styles.tokenSecondRow}>
+                                    <span>Всего заимствовано: ? из ?</span>
+                                    <span>APY, variable: 3,8%</span>
+                                </div>
+                                <div className={styles.tokenThirdRow}>
+                                    <span>Коэффициент использования: ?%</span>
+                                </div>
+                            </div>
+                            <a className={styles.boxClose} href="#">x</a>
+                        </div>
+                    </div>
+                    <div className={styles.tokenInfo} id="popup-box3">
+                        <div className={styles.popupContent} >
+                            <h4><Image src={"technologies/usdt.svg"} width={35} height={35}/>  USDT</h4>
+                            
+                            <div className={styles.tokenTextInfo}>
+                                <div className={styles.tokenFirstRow}>
+                                    <span>Всего вложено: ? из ?</span>
+                                    <span>APR: 1,78% </span>
+                                </div>
+                                <div className={styles.tokenSecondRow}>
+                                    <span>Всего заимствовано: ? из ?</span>
+                                    <span>APY, variable: 2,1%</span>
+                                </div>
+                                <div className={styles.tokenThirdRow}>
+                                    <span>Коэффициент использования: ?%</span>
+                                </div>
+                            </div>
+                            <a className={styles.boxClose} href="#">x</a>
+                        </div>
+                    </div>
+                    
+                    {/* ВЛОЖЕНИЕ АКТИВА     ВЛОЖЕНИЕ АКТИВА     ВЛОЖЕНИЕ АКТИВА */}
+                    <div className={styles.tokenInfo} id="popup-box4">
+                        <div className={styles.popupContent}>
+                            <h4>Введите сумму которую хотите вложить:</h4>
+                            <div className={styles.supplyForm}>
+                                <input type="text" pattern="[0-9]" placeholder="0.00 BTC" className={styles.supplyInput} onChange={(e) => {setSupplyValue(e.target.value)}}></input>
+                                <button onClick={postBTCSupply}>Вложить</button>
+                            </div>
+                            <a className={styles.boxClose} href="#">x</a>
+                        </div>
+                    </div>
+
+                    <div className={styles.tokenInfo} id="popup-box5">
+                        <div className={styles.popupContent}>
+                            <h4>Введите сумму которую хотите вложить:</h4>
+                            <div className={styles.supplyForm}>
+                                <input type="text" pattern="[0-9]" placeholder="0.00 ETH" className={styles.supplyInput} onChange={(e) => {setSupplyValue(e.target.value)}}></input>
+                                <button onClick={postETHSupply}>Вложить</button>
+                            </div>
+                            <a className={styles.boxClose} href="#">x</a>
+                        </div>
+                    </div>
+
+                    <div className={styles.tokenInfo} id="popup-box6">
+                        <div className={styles.popupContent}>
+                            <h4>Введите сумму которую хотите вложить:</h4>
+                            <div className={styles.supplyForm}>
+                                <input type="text" pattern="[0-9]" placeholder="0.00 USDT" className={styles.supplyInput} onChange={(e) => {setSupplyValue(e.target.value)}}></input>
+                                <button onClick={postUSDTSupply}>Вложить</button>
+                            </div>
+                            <a className={styles.boxClose} href="#">x</a>
+                        </div>
                     </div>
                 </div>
             ) : (
@@ -196,9 +305,12 @@ export async function getServerSideProps() {
     const posts = await prisma.post.findMany()
     const res = await fetch('https://api.coingecko.com/api/v3/simple/price?ids=bitcoin%2Cethereum%2Cusdt&vs_currencies=usd')
     const filteredCoins = await res.json()
+    const supplies = await prisma.activeSupplies.findMany()
+
     return {
         props: {
             posts: JSON.parse(JSON.stringify(posts)),
+            supplies: JSON.parse(JSON.stringify(supplies)),
             filteredCoins
         }
     }
--- src/pages/staking.js
+++ src/pages/staking.js
@@ -6,16 +6,19 @@ import styles from '../styles/Staking.module.scss'
 import Image from 'next/image'
 import AppHeader from '../../components/appHeader'
 
-export default function Staking({filteredCoins}) {
+export default function Staking({filteredCoins, stakes}) {
 
+    const { data: session, status } = useSession()
+    const currUser = session?.user?.userID
+    console.log(session?.user?.userID)
     const [ethPrice, setEthPrice] = useState(0)
     const [value, setValue] = useState(1)
     var today = new Date();
-    var today1 = new Date()
+    var today1 = new Date();
     var dd = String(today.getDate()).padStart(2, '0');
     var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
     var yyyy = today.getFullYear();
-
+    var stakeAPR = 3.2
 
     var newDD = today.getDate() + 5;
 
@@ -33,6 +36,57 @@ export default function Staking({filteredCoins}) {
     var nextSixMonth = dd + '.' + sixMonth + "." + oneYear
     var nextThreeMonth = dd + '.' + threeMonth + "." + oneYear
     var nextOneMonth = dd + '.' + oneMonth + "." + oneYear
+    var currDateTwoYears = new Date(yyyy+2, mm, dd)
+    var currDateYearAndHalf = new Date(yyyy+1 , today1.getMonth() + 6, dd)
+    var currDateOneYear = new Date(yyyy+1, mm, dd);
+    var currDateSixMonth = new Date(yyyy, today1.getMonth() + 6, dd)
+    var currDateThreeMonth = new Date(yyyy, today1.getMonth() + 3, dd)
+    var currDateOneMonth = new Date(yyyy, today1.getMonth() + 1, dd)
+
+
+    const addStakeToUser = async({userID=currUser, amount=parseFloat(ethPrice), startedAt=today1, blockDuration, blockedUntil}) => {
+        console.log("VAlue is " + value)
+        console.log("Type is" + typeof(value))
+        switch (value) {
+            case "1":
+                blockDuration = "2 года"
+                blockedUntil = currDateTwoYears
+                break;
+            case "2":
+                blockDuration = "1,5 года"
+                blockedUntil = currDateYearAndHalf
+                break;
+            case "3":
+                blockDuration = "1 год"
+                blockedUntil = currDateOneYear
+                break;
+            case "4":
+                blockDuration = "6 месяцев"
+                blockedUntil = currDateSixMonth
+                break;
+            case "5":
+                blockDuration = "3 месяца"
+                blockedUntil = currDateThreeMonth
+                break;
+            case "6":
+                blockDuration = "1 месяц"
+                blockedUntil = currDateOneMonth
+                break;
+            default:
+                blockDuration = "1 месяц"
+                blockedUntil = currDateOneMonth
+                break;
+        }
+        const {data} = await axios.post('api/stakes', {
+            userID,
+            amount,
+            startedAt,
+            blockDuration,
+            blockedUntil
+        })
+        location.reload()
+        console.log(data)
+    }
     
     return (
         <div className={styles.body}>
@@ -75,13 +129,64 @@ export default function Staking({filteredCoins}) {
                                 {value==5 && <> {nextThreeMonth}</>}
                                 {value==6 && <> {nextOneMonth}</>}
                                 </p>
+                                <p>Годовая процентная ставка: {stakeAPR}%</p>
                                 <p>Плата за обслуживание: ~{((ethPrice * [filteredCoins][0].ethereum.usd)*0.03).toFixed(3)}$</p>
                             </div>
-                            <div className={styles.stakeButton}>
+                            <div className={styles.stakeButton} onClick={addStakeToUser}>
                                 Stake
                             </div>
                         </div>
-                        <div id="unstake" className={styles.tabs_block}>Unstake. Sunt cillum minim nostrud ipsum laboris in reprehenderit consectetur sunt dolor ad quis elit sint. Nulla ea Lorem sint eiusmod nulla. Ut cupidatat enim qui anim eu irure occaecat occaecat nisi.</div>
+                        <div id="unstake" className={styles.tabs_block}>
+                            <h3>Unstake</h3>
+                            <div className={styles.stakeList}>
+                                <ul>
+                                    {stakes?.map(stake => (
+                                    stake.userID == currUser && <li key={stake.stakeID}>
+                                        <span>{stake.amount} ETH</span>
+                                        <span>{stake.blockDuration}</span>
+                                        {stake.blockDuration >= today && <button>Unstake</button>}
+                                        {stake.blockDuration < today && <button disabled>Unstake</button> }
+                                    </li> 
+                                    
+                                    ))}
+                                    
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className={styles.faq}>
+                    <h2>F.A.Q</h2>
+                    <div className={styles.faqItem}>
+                        <label className={styles.faqTitle} for="faq1">
+                            Что такое стейкинг?
+                        </label>
+                        <input type="checkbox" className={styles.faqInput} name="faq" id="faq1"/>
+                        <div className={styles.faqText}>
+                            Стекинг — это способ пассивного заработка, при котором пользователи хранят монеты на алгоритме Proof of Stake (PoS) и обеспечивают работоспособность блокчейна. 
+                            Это дает им право получать прибыль.
+                        </div>
+                    </div>
+
+                    <div className={styles.faqItem}>
+                        <label className={styles.faqTitle} for="faq2">
+                            Что такое APY?
+                        </label>
+                        <input type="checkbox" className={styles.faqInput} name="faq" id="faq2"/>
+                        <div className={styles.faqText}>
+                            APY – это годовая ставка прибыли от инвестиций с учётом сложных процентов, которые накапливаются или растут вместе с балансом.
+                            Сложные проценты включают проценты, полученные от первоначального депозита, плюс проценты, полученные с этих процентов.
+                        </div>
+                    </div>
+                    <div className={styles.faqItem}>
+                        <label className={styles.faqTitle} for="faq3">
+                            Чем отличается APY от APR?
+                        </label>
+                        <input type="checkbox" className={styles.faqInput} name="faq" id="faq3"/>
+                        <div className={styles.faqText}>
+                        В отличие от APR, которая учитывает только обычные проценты, APY включает сложные проценты. Сложные проценты - это сумма, полученная на проценты и основную сумму вклада. Вот почему APY более выгоден, чем APR.
+                        Инвесторы могут заработать APY, размещая свои токены и используя доходный фарминг для обеспечения пулов ликвидности. Они также могут заработать APY, храня свои токены на сберегательных счетах.
+                        </div>
                     </div>
                 </div>
             </div>
@@ -93,8 +198,10 @@ export default function Staking({filteredCoins}) {
 export async function getServerSideProps() {
     const res = await fetch('https://api.coingecko.com/api/v3/simple/price?ids=bitcoin%2Cethereum%2Cusdt&vs_currencies=usd')
     const filteredCoins = await res.json()
+    const stakes = await prisma.activeStake.findMany()
     return {
         props: {
+            stakes: JSON.parse(JSON.stringify(stakes)),
             filteredCoins
         }
     }
